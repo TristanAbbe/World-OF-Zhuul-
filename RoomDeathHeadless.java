4391540da577746d9cf9229aabf4aeaabaab1db1
Mazurie Jules <jules.mazurie@gmail.com>
2024-01-07 21:42:00 +0100

    Création room des différentes morts

---
+++ RoomDeathHeadless.java
@@ -0,0 +1,15 @@
+/**
+ * Décrivez votre classe roomDeath ici.
+ *
+ * @author (votre nom)
+ * @version (un numéro de version ou une date)
+ */
+public class RoomDeathHeadless extends Room
+{
+
+    public RoomDeathHeadless(){
+    
+        super("This is the end youre are dead", "The END Headless","ImagesAliceRedimmensionnées/Mort_Partie_perdue_Guillotinée.png",30);
+    }
+
+}
