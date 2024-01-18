d16ebded2562607d16d48a40e2c3565fb3e55634
Corentin Delpirou <delpirou.corentin@gmail.com>
2024-01-18 02:07:41 +0100

    Ajout de la javadoc pour la classe Main

--- Main.java
+++ Main.java
@@ -1,11 +1,17 @@
 
 /**
- * Décrivez votre classe Main ici.
+ * The Main class is the entry point for the Alice in Wonderland program.
+ * It initializes the game and the GUI, thus starting the execution of the game.
  *
  * @author ABBE Tristan, ANET Janelle, DELPIROU Corentin, MAZURIE Jules, PERSONNE Germain, RIVIERE Jade
  * @version 1.0
  */
 public class Main {
+    /**
+     * The program's main method.
+     * It creates an instance of the AliceInWonderlandGame class, initializes a new game,
+     * then creates an instance of the GUI class to start the graphical interface.
+     */
     public static void main(String[] args) {
         // Create an instance of AliceInWonderlandGame
         AliceInWonderlandGame game = new AliceInWonderlandGame();
